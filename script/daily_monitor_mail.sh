e8990c7fc5fdf4c9d6bb21d766c5abc02711a05b
Sandip <sandip22c@yahoo.co.in>
2017-02-21 16:14:31 +0530

    Update daily_monitor_mail.sh

--- script/daily_monitor_mail.sh
+++ script/daily_monitor_mail.sh
@@ -2,4 +2,4 @@
 
 PREV_DATE=$(date +'%Y-%m-%d' --date='1 day ago')
 
-mutt -s "Load average report for $PREV_DATE" -a /var/tmp/monitor.log.$PREV_DATE gurjinder@fnp.in -c ankur.dinesh@wirefootindia.com -c fnpsupport@anmsoft.com <  /dev/null
+mutt -s "Load average report for $PREV_DATE" -a /var/tmp/monitor.log.$PREV_DATE   <  /dev/null
